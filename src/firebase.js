139cfdc0f531efb283ae765b088a9783049ffdec
Edwin Joseph <63284250+edjo241@users.noreply.github.com>
2021-08-06 10:57:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase'
-const firebaseConfig = {
-    apiKey: "AIzaSyCMhSQbWKkOnHnEdP_4GBjjEJnZpn66wLk",
-    authDomain: "messagify-c1cc5.firebaseapp.com",
-    projectId: "messagify-c1cc5",
-    storageBucket: "messagify-c1cc5.appspot.com",
-    messagingSenderId: "443222605673",
-    appId: "1:443222605673:web:6b2ec8c430099dfe2387f8",
-    measurementId: "G-ERKZ5HM606"
-  };
+
+
 
   const firebaseApp=firebase.initializeApp(firebaseConfig);
   const db=firebaseApp.firestore();
